6a40ab6ed505a8c449f416beaf93c96092fa76f8
Erzat <syltanoverzat@gmail.com>
2025-05-30 13:08:30 +0600

    Update News.tsx

--- src/pages/News.tsx
+++ src/pages/News.tsx
@@ -6,7 +6,7 @@ const newsItems = [
   {
     id: 1,
     title: "Assassin's Creed Shadows стала крупнейшим запуском в истории франшизы",
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1r71qpReenMce5zNVrCPA9Qe-TLvWoU8Z4A&s",
+    image: "https://cms-assets.xboxservices.com/assets/f7/75/f7759371-ffd8-429c-b858-e3e3242fa547.jpg?n=343859994_GLP-Page-Hero-1084_1920x1080.jpg",
     date: "20 мая 2025",
     excerpt: "Более 1 миллиона игроков в день релиза и 3 миллиона за первую неделю. Игра возглавила чарты продаж в США и Великобритании и превзошла Assassin's Creed Odyssey по доходам и числу игроков.",
     link: "/news/assassins-creed-shadows-launch"
@@ -22,7 +22,7 @@ const newsItems = [
   {
     id: 3,
     title: "GTA VI официально перенесена на май 2026 года",
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0nL02gaXhUjay-gxmseHkfJJG1PjhrOmoPDFeXJ5n9tGp0Xly3vyH0zxkkF41nCMHlh4&usqp=CAU",
+    image: "https://media.riamo.ru/get_resized/Sk-pjIp3N7ZO5mMLhgnMdmW-AMo=/1920x1080/filters:rs(fill-down):format(webp)/YXJ0aWNsZXMvaW1hZ2UvMjAyNS81L2ltYWdlLnBuZw.webp",
     date: "17 мая 2025",
     excerpt: "Rockstar Games объявила о переносе релиза GTA VI. Компания сообщает, что дополнительное время необходимо для доработки игры до нужного уровня качества.",
     link: "/news/gta-vi-delayed"
@@ -46,7 +46,7 @@ const newsItems = [
   {
     id: 6,
     title: "DOOM: The Dark Ages анонсирован на QuakeCon",
-    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTHhmEKj3lyMZiG7oQdWSFqlQ3mWa4CVxNOWKSUVGN7Uagh6_AwMG9YmTMHPKHisX2W4oc&usqp=CAU",
+    image: "https://image.api.playstation.com/vulcan/ap/rnd/202501/1405/bb3f89ae3425f3aa86041ff71646fc5d44d7705f3a383427.jpg",
     date: "5 мая 2025",
     excerpt: "id Software представила новую часть культовой серии шутеров. Действие игры развернется в альтернативном средневековье.",
     link: "/news/doom-dark-ages-announced"
@@ -63,7 +63,7 @@ const News = () => {
           <div className="mb-12 border-b border-border pb-6">
             <h1 className="text-3xl md:text-4xl font-bold mb-4">Новости игровой индустрии</h1>
             <p className="text-muted-foreground text-lg max-w-3xl">
-              Самые свежие новости о релизах, анонсах, обновлениях и событиях в мире видеоигр.
+              Самые свежие новости  релизах, анонсах, обновлениях и событиях в мире видеоигр.
             </p>
           </div>
 
